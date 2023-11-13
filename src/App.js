fb6e8cb7ca44b202d57f659e9885eb27e75f7698
hsh-newbi <hsonghart0426@gmail.com>
2023-11-13 16:07:23 +0900

    리액트 03 숙제 - useState에서 state를 변경하는 함수를 이용하여 글 제목 바꾸기

--- src/App.js
+++ src/App.js
@@ -2,16 +2,40 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let post = "강남 우동 맛집";
-  let [item, b] = useState(["남자 코트 추천", "강남 우동 맛집", "파이썬독학"]);
+  let [item, setItem] = useState([
+    "남자 코트 추천",
+    "강남 우동 맛집",
+    "파이썬독학",
+  ]);
+  let [good, setGood] = useState(0);
+  const title = "여자 추천 코트";
+
+  const set = () => {
+    let copy = [...item];
+    copy[0] = "여자 추천 코트";
+    setItem(copy);
+  };
 
   return (
     <div className="App">
       <div className="nav-bar">
         <h4>블로그 제목</h4>
       </div>
+      <button type="button" onClick={set}>
+        제목 변경
+      </button>
       <div className="list">
-        <h4>{item[0]}</h4>
+        <h4>
+          {item[0]}
+          <span
+            onClick={() => {
+              setGood(good + 1);
+            }}
+          >
+            ❤️
+          </span>
+          {good}
+        </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
