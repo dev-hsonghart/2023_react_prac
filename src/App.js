3b6d0a9739fe27d7966a8e0f844f7eca46e24dc2
hsh-newbi <hsonghart0426@gmail.com>
2023-11-13 17:46:37 +0900

    리액트 06 숙제 - 제목배열수만큼 좋아요 값 배열이 만들어지게 함

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
     "강남 우동 맛집",
     "파이썬독학",
   ]);
-  let [good, setGood] = useState([0, 0, 0]);
+  let goods = item.map(function () {
+    return 0;
+  });
+
+  let [good, setGood] = useState(goods);
 
   const set = () => {
     let copy = [...item];
@@ -37,24 +41,6 @@ function App() {
       <button type="button" onClick={set}>
         제목 변경
       </button>
-      {/* <div className="list">
-        <h4>
-          {item[0]}
-          <span
-            onClick={() => {
-              setGood(good + 1);
-            }}
-          >
-            ❤️
-          </span>
-          {good}
-        </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4 onClick={controlTitle}>{item[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div> */}
 
       {item.map(function (item, i) {
         return (
