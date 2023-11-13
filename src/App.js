d8694ad733780632df3e18fbec1c10d95c710093
hsh-newbi <hsonghart0426@gmail.com>
2023-11-13 15:14:08 +0900

    리액트 02 숙제 - useState를 이용하여 따로 저장된 변수를 html에 출력하기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   let post = "강남 우동 맛집";
+  let [item, b] = useState(["남자 코트 추천", "강남 우동 맛집", "파이썬독학"]);
 
   return (
     <div className="App">
       <div className="nav-bar">
         <h4>블로그 제목</h4>
       </div>
-      <h4>{post}</h4>
+      <div className="list">
+        <h4>{item[0]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{item[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{item[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
